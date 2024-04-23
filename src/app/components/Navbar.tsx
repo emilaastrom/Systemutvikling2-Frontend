b2ac563945ee74d0c5906caaba19044ca172c934
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-04-23 15:54:34 +0200

    style: :lipstick: Replace emoji with icons from Heroicons

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { motion } from "framer-motion";
 import NavbarButton from "./Navbarbutton";
 import { Alumni_Sans_Collegiate_One } from "next/font/google";
+import SvgIcon from "./icons/CustomIcon";
+
 const Navbar = () => {
   const navbarVariants = {
     hidden: { opacity: 0, y: 50 },
@@ -13,11 +15,111 @@ const Navbar = () => {
     <div>
       {/* Desktop Navbar */}
 
-      <div className="fixed left-0 top-1/2 z-10 hidden md:flex transform -translate-y-1/2 h-70 w-auto flex-col gap-4 text-black p-5 border-r-2 border-slate-200">
-        <NavbarButton to="/">🏠 Hjem</NavbarButton>
-        <NavbarButton to="/login">💭 Test Login</NavbarButton>
-        <NavbarButton to="/savings">💰 Sparing</NavbarButton>
-        <NavbarButton to="/profile">👤 Konto</NavbarButton>
+      <div className="fixed left-0 top-1/2 z-10 hidden md:flex transform -translate-y-1/2 h-70 flex-col gap-4 text-black p-5 border-r-2 border-slate-200">
+        <div className="flex flex-row">
+          <NavbarButton to="/">
+            <SvgIcon
+              className={"h-5 w-5"}
+              svg={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
+              }
+            />
+            <span className="px-2">Hjem</span>
+          </NavbarButton>
+        </div>
+
+        <div className="flex flex-row">
+          <NavbarButton to="/login">
+            <SvgIcon
+              className={"h-5 w-5"}
+              svg={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z"
+                  />
+                </svg>
+              }
+            />
+            <span className="px-2">Login</span>
+          </NavbarButton>
+        </div>
+
+        <div className="flex flex-row">
+          <NavbarButton to="/statistics">
+            <SvgIcon
+              className={"h-5 w-5"}
+              svg={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
+                  />
+                </svg>
+              }
+            />
+            <span className="px-2">Sparing</span>
+          </NavbarButton>
+        </div>
+
+        <div className="flex flex-row">
+          <NavbarButton to="/profile">
+            <SvgIcon
+              className={"h-5 w-5"}
+              svg={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              }
+            />
+            <span className="px-2">Konto</span>
+          </NavbarButton>
+        </div>
       </div>
 
       {/* Mobile Navbar */}
@@ -33,30 +135,115 @@ const Navbar = () => {
       >
         <div className="flex flex-col items-center">
           <div className="flex">
-            <NavbarButton to="/">🏠</NavbarButton>
+            <NavbarButton to="/">
+              <SvgIcon
+                className={"h-5 w-5"}
+                svg={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                    />
+                  </svg>
+                }
+              />
+              <span className="flex justify-center text-xs pt-1 w-8">HJEM</span>
+            </NavbarButton>
           </div>
-          <span className="flex justify-center text-xs ">HJEM</span>
         </div>
 
         <div className="flex flex-col items-center">
           <div className="flex">
-            <NavbarButton to="/about">💭</NavbarButton>
+            <NavbarButton to="/login">
+              <SvgIcon
+                className={"h-5 w-5"}
+                svg={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z"
+                    />
+                  </svg>
+                }
+              />
+              <span className="flex justify-center text-xs pt-1 w-8">LOGIN</span>
+            </NavbarButton>
           </div>
-          <span className="flex justify-center text-xs ">OM</span>
         </div>
 
         <div className="flex flex-col items-center">
           <div className="flex">
-            <NavbarButton to="/savings">💰</NavbarButton>
+            <NavbarButton to="/savings">
+              <SvgIcon
+                className={"h-5 w-5"}
+                svg={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
+                    />
+                  </svg>
+                }
+              />
+              <span className="flex justify-center text-xs pt-1 w-8">SPARING</span>
+            </NavbarButton>
           </div>
-          <span className="flex justify-center text-xs ">SPARING</span>
         </div>
 
         <div className="flex flex-col items-center">
           <div className="flex">
-            <NavbarButton to="/profile">👤</NavbarButton>
+            <NavbarButton to="/profile">
+              <SvgIcon
+                className={"h-5 w-5"}
+                svg={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                }
+              />
+              <span className="flex justify-center text-xs pt-1 w-8">KONTO</span>
+            </NavbarButton>
           </div>
-          <span className="flex justify-center text-xs ">KONTO</span>
         </div>
       </motion.div>
     </div>
