9b7a5202e06c23ac67e4cd553869301d127d5b03
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-04-23 13:16:07 +0200

    feat: :wheelchair: Add autogenerated (and optionally customized) aria-labels to all navbar buttons

--- src/app/components/Navbarbutton.tsx
+++ src/app/components/Navbarbutton.tsx
@@ -3,18 +3,33 @@ import React from "react";
 interface NavbarButtonProps {
   to: string;
   children: React.ReactNode;
+  ariaLabel?: string; // Optional custom aria-label
 }
 
-const NavbarButton = ({ to, children }: NavbarButtonProps) => {
+{
+  /* Navbar button that navigates to a given path.
+The aria-label is a prop that increases accessibility when using screen readers.
+We check for "/" and replace it with 'Hjemmesiden'. 
+If not provided the button defaults the aria-label to "Navigate to {to} page" */
+}
+const NavbarButton = ({ to, children, ariaLabel }: NavbarButtonProps) => {
+  const finalAriaLabel = ariaLabel
+    ? ariaLabel
+    : to === "/"
+    ? "Naviger til hjemmesiden"
+    : `Naviger til ${to.slice(1)} siden`;
+
   return (
     <a
-        href={to}
-        className="text-left py-2 px-4 rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]
+      href={to}
+      className="text-left py-2 px-4 rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]
         bg-white bg-opacity-99 border-solid border-gray-200 
         hover:border-mantis-300 hover:text-black hover:scale-105 hover:bg-color-mantis-100
-        transition-all duration-300 ease-in-out border-2"
+        transition-all duration-300 ease-in-out border-2
+        "
+      aria-label={finalAriaLabel}
     >
-        <span>{children}</span>
+      <span>{children}</span>
     </a>
   );
 };
