8421e46753fde61cd027b0a9ad2b6bf1cf1c5e6c
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-04-24 10:43:51 +0200

    style: :bento: Restyling navbarbuttons and including pathname check

--- src/app/components/Navbarbutton.tsx
+++ src/app/components/Navbarbutton.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 interface NavbarButtonProps {
   to: string;
@@ -7,27 +8,35 @@ interface NavbarButtonProps {
 }
 
 {
-  /* Navbar button that navigates to a given path.
-The aria-label is a prop that increases accessibility when using screen readers.
-We check for "/" and replace it with 'Hjemmesiden'. 
-If not provided the button defaults the aria-label to "Navigate to {to} page" */
+// Navarbutton component, with a to prop, children prop and an optional aria-label prop
 }
 const NavbarButton = ({ to, children, ariaLabel }: NavbarButtonProps) => {
+
+    /* The aria-label is a prop that increases accessibility when using screen readers.
+  We are checking for "/" and replacing it with 'Hjemmesiden'. 
+  If not provided the button defaults the aria-label to "Naviger til {to} siden" */
   const finalAriaLabel = ariaLabel
     ? ariaLabel
     : to === "/"
     ? "Naviger til hjemmesiden"
     : `Naviger til ${to.slice(1)} siden`;
 
+  const pathname = usePathname(); // Declare the correct type for the pathname object
+
+  const baseStyles = "flex md:flex-row md:min-w-32 flex-col py-2 px-4 rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]";
+  const activeStyles = "bg-white border-black text-black dark:bg";
+  const inactiveStyles = "bg-slate-100 border-slate-300 text-slate-700";
+  const hoverStyles = "hover:border-black hover:text-slate-700 hover:scale-110 hover:bg-white transition-all duration-300 ease-in-out border-2";
+  const darkStyles = "dark:bg-slate-800 dark:selection:bg-border-red-500 dark:border-slate-300 dark:text-white dark:hover:border-white";
+
+  const className = `${baseStyles} ${
+    pathname === to ? activeStyles : inactiveStyles
+  } ${hoverStyles} ${darkStyles}`;
+
   return (
     <a
       href={to}
-      className="flex md:flex-row md:min-w-32 flex-col py-2 px-4 rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]
-        bg-white bg-opacity-99 border-solid border-gray-200 
-        hover:border-slate-400 hover:text-black hover:scale-110
-        transition-all duration-300 ease-in-out border-2
-        dark:bg-slate-800 dark:border-slate-300 dark:text-white dark:hover:border-mantis-300
-        "
+      className={ className }
       aria-label={finalAriaLabel}
     >
       <div className="flex flex-col items-center md:flex-row">{children}</div>
