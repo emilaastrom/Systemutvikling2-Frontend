5fb2bd7495953868bd913ca1d96c89e7e4aaf194
bragetn <brage.naustan@gmail.com>
2024-04-24 12:49:28 +0200

    style: 🎨 Improved new challenge styling

--- src/app/components/challenges/ChallengecardAddModal.tsx
+++ src/app/components/challenges/ChallengecardAddModal.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CustomIcon from '../icons/CustomIcon';
 
 type ChallengecardModalProps = {
     onClose: () => void;
@@ -41,13 +42,13 @@ const ChallengecardAddModal: React.FC<ChallengecardModalProps> = ({ onClose }) =
         const suggestion = suggestions[currentPage];
 
         return (
-            <div className="mx-10 border-2 rounded-md p-4  cursor-pointer bg-green-100 hover:bg-green-200 flex flex-col justify-between h-2/3">
+            <div className="border-2 rounded-md p-4  cursor-pointer overflow-y-auto bg-white hover:bg-[#fff] flex flex-col justify-between w-3/4 h-full">
                 <span className="text-gray-500 text-sm text-start">
                     <span className="font-semibold">Kategori: </span> {suggestion.category}
                     </span>
-                <div className="flex flex-col px-16 justify-center items-center mb-2 overflow-y-auto">
-                    <h3 className="text-3xl font-semibold text-center mb-5">{suggestion.title}</h3>
-                    <p className="text-lg text-gray-700 text-center mb-2">{suggestion.description}</p>
+                <div className="flex flex-col md:px-16 x-4 justify-center items-center mb-2 ">
+                    <h3 className="text-xl font-semibold text-center mb-5">{suggestion.title}</h3>
+                    <p className="text-md text-gray-700 text-center mb-2">{suggestion.description}</p>
                 </div>
                 
                 
@@ -60,11 +61,11 @@ const ChallengecardAddModal: React.FC<ChallengecardModalProps> = ({ onClose }) =
 
     return (
         <div onClick={onClose} className="fixed h-screen w-screen inset-0 z-30 flex items-center justify-center bg-black bg-opacity-50">
-            <div onClick={stopPropagation} className="bg-white rounded-lg w-screen md:w-1/2 h-2/3 m-5 overflow-auto">
+            <div onClick={stopPropagation} className="bg-white rounded-lg w-screen md:w-1/2 h-4/5 m-5 overflow-auto">
                 <div className="bg-fuchsia-200 p-4 rounded-t-lg w-full max-h-1/4 text-center flex flex-col justify-center items-center font-semibold overflow-auto">
                     Ny utfordring?
                 </div>              
-                <div className="flex justify-center mt-4 mx-4 text-lg ">
+                <div className="flex justify-center m-4 text-lg ">
                     <div className="flex items-center mr-4">
                         <input
                             type="radio"
@@ -100,25 +101,42 @@ const ChallengecardAddModal: React.FC<ChallengecardModalProps> = ({ onClose }) =
                     </div>
                 </div>
 
-                <div className="flex justify-between mx-4 mb-4 font-semibold">
+                <div className="flex justify-between h-2/3 mb-4 font-semibold">
                     <button 
                         onClick={() => setCurrentPage(Math.max(0, currentPage - 1))} 
                         disabled={currentPage === 0}
                         className={`mx-1 px-4 py-2 font-bold ${currentPage === 0 ? "opacity-50 cursor-not-allowed" : ""}`}
                     >
-                        Prev
+                        <CustomIcon 
+                            className={""}
+                            svg={
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                                </svg>
+                            }
+                         />
                     </button>
-                    
+                    {renderSuggestion()}
                     <button 
                         onClick={() => setCurrentPage(Math.min(totalPages - 1, currentPage + 1))} 
                         disabled={currentPage === totalPages - 1}
                         className={`mx-1 px-4 py-2 font-bold ${currentPage === totalPages - 1 ? "opacity-50 cursor-not-allowed" : ""}`}
                     >
-                        Next
+                        <CustomIcon 
+                            className={""}
+                            svg={
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                                </svg>
+                            }
+                         />
                     </button>
                 </div>
-            
-                {renderSuggestion()}
+
+                <div className="flex justify-center items-center">
+                    <button className="border-2 border-green-200 p-5">Add challenge</button>
+                </div>
+
             </div>
         </div>
     );
