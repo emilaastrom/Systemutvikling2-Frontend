583f4b3a25af92bf043fbc3d1fd9c504bdbdd69b
Torgeir Haukaas <haukaas.torgeir@gmail.com>
2024-04-25 15:18:23 +0200

    api: add support for dev servers

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useAuth } from "@/context/AuthContext";
 
-const host = "http://localhost";
-const api = "";
-// const api = "/api/v1";
+// const host = "http://localhost";
+const https = "https://"
+const host = ".sysdevservices.tech/";
 
 const serviceInfo = {
   auth: { port: 8111, path: "/auth" },
@@ -19,7 +19,7 @@ type Info = {
 };
 
 function getAuthToken() {
-  return "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZGVmNTU1My02Mzg4LTQ1OTYtODViMS00Njg3MDdlZWYyYzUiLCJpYXQiOjE3MTM5NTA1NTEsImV4cCI6MTcxMzk4NjU1MX0.thmhOHq3QVeZjv8aAPixTyQC5H4VPV7gKpu6sjLipzc";
+  return "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYjE2MzRmNC1lMzRhLTQ0NTMtYjgzNS04NTdlZTE4ZDViNmEiLCJpYXQiOjE3MTQwMzA0NDMsImV4cCI6MTcxNDA2NjQ0M30.HTDyw32mCeWjOz-tt93asrfvD03ouETOxg_6ZOKXH9I";
   // const { token } = useAuth();
   // return token;
 }
@@ -28,7 +28,7 @@ const token = getAuthToken();
 
 const createAxiosService = (info, timeout = 1000) => {
   const service = axios.create({
-    baseURL: `${host}:${info.port}${api}${info.path}`,
+    baseURL: `${host}:${info.port}${info.path}`,
     headers: {
       "Content-Type": "application/json",
     },
@@ -42,20 +42,20 @@ const createAxiosService = (info, timeout = 1000) => {
       }
       return config;
     },
-    (error) => Promise.reject(error)
+    (error) => Promise.reject(error),
   );
 
   return service;
 };
 
 function ApiHandler(service, method, url, data = null) {
-  const config = data ? [service + url, data] : [service + url];
+  const config = data ? [https + service + host + service + url, data] : [https + service + host + service + url];
   return gatewayService[method](...config)
     .then((response) => response.data)
     .catch((error) => {
       console.error(
         `Failed to ${method === "get" ? "fetch" : "post"}: ${url}`,
-        error
+        error,
       );
       throw error;
     });
@@ -63,6 +63,4 @@ function ApiHandler(service, method, url, data = null) {
 
 const gatewayService = createAxiosService(serviceInfo.gateway);
 
-export {
-  ApiHandler,
-};
+export { ApiHandler };
