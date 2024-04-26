485339a67d1295b2b0997c6b23174b42b94e5412
Torgeir Haukaas <haukaas.torgeir@gmail.com>
2024-04-26 11:27:20 +0200

    api: properly use react hooks

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,37 +2,23 @@ import axios from "axios";
 import { useAuth } from "@/context/AuthContext";
 
 // const host = "http://localhost";
-const https = "https://"
+const https = "https://";
 const host = ".sysdevservices.tech/";
-
-const serviceInfo = {
-  auth: { port: 8111, path: "/auth" },
-  goal: { port: 8081, path: "/goal" },
-  bank: { port: 8082, path: "/bank" },
-  user: { port: 8083, path: "/user" },
-  challenge: { port: 8084, path: "/challenge" },
-  gateway: { port: 8080, path: "" },
-};
-type Info = {
-  port: number;
-  path: string;
-};
 
 function getAuthToken() {
   return "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYjE2MzRmNC1lMzRhLTQ0NTMtYjgzNS04NTdlZTE4ZDViNmEiLCJpYXQiOjE3MTQwMzA0NDMsImV4cCI6MTcxNDA2NjQ0M30.HTDyw32mCeWjOz-tt93asrfvD03ouETOxg_6ZOKXH9I";
-  // const { token } = useAuth();
-  // return token;
 }
 
-const token = getAuthToken();
+export const useApiService = () => {
+  const  token  = useAuth();
 
-const createAxiosService = (info, timeout = 1000) => {
   const service = axios.create({
-    baseURL: `${host}:${info.port}${info.path}`,
+    baseURL: `${host}:8080`,
     headers: {
       "Content-Type": "application/json",
+      // ...(token ? { Authorization: `Bearer ${token}` } : {}),
     },
-    timeout,
+    timeout: 1000,
   });
 
   service.interceptors.request.use(
@@ -48,19 +34,22 @@ const createAxiosService = (info, timeout = 1000) => {
   return service;
 };
 
-function ApiHandler(service, method, url, data = null) {
-  const config = data ? [https + service + host + service + url, data] : [https + service + host + service + url];
-  return gatewayService[method](...config)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.error(
-        `Failed to ${method === "get" ? "fetch" : "post"}: ${url}`,
-        error,
-      );
+export const useApiHandler = () => {
+  const gatewayService = useApiService();
+
+  const apiHandler = async (service, method, url, data = null) => {
+    const path = https + service + host + service + url;
+    const config = data
+      ? [path, data]
+      : [path];
+
+    try {
+      const response = await gatewayService[method](...config);
+      return response.data;
+    } catch (error) {
+      console.error(`Failed to ${method}: ${url}`, error);
       throw error;
-    });
-}
-
-const gatewayService = createAxiosService(serviceInfo.gateway);
-
-export { ApiHandler };
+    }
+  };
+  return apiHandler;
+};
