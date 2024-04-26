973cf0908dc5aaec1a04d96a7ad2ae0c7d802445
Markus Dalbakk <markueda@stud.ntnu.no>
2024-04-26 10:32:26 +0200

    home-page: Implemented color palette in homepage

--- src/app/components/Goalpig.tsx
+++ src/app/components/Goalpig.tsx
@@ -18,7 +18,7 @@ const Goalpig: React.FC<GoalpigProps> = ({ current, max, goal }) => {
     const clipPath = `polygon(0% 100%, 100% 100%, 100% ${100 - percentage}%, 0% ${100 - percentage}%)`;
 
     return (
-        <div className="font-semibold items-center md:mt-5 mt-3 md:mb-5 mb-3 text-gray-800 text-center relative">
+        <div className="font-semibold items-center md:mt-5 mt-3 md:mb-5 mb-3 text-dark text-center relative">
             <div className='md:text-4xl text-3xl font-custom1'>
                 {goal}
             </div>
--- src/app/components/path/Checkpoint.tsx
+++ src/app/components/path/Checkpoint.tsx
@@ -1,8 +1,9 @@
+import { colors } from "../../../../tailwind.config";
 import { Vector } from "@/util/types/vector";
 
 export default function Checkpoint({ passed, coords, scale, size, borderWidth }: { passed: boolean; coords: Vector, scale: number, size: number, borderWidth: number}) {
-    const color = passed ?  "bg-green-400" : "bg-gray-200";
-    const hoverColor = passed ?  "hover:bg-green-500" : "hover:bg-gray-300";
+    const color = passed ? "bg-primary-light" : "bg-secondary-light";
+    const hoverColor = passed ?  "hover:bg-primary-dark" : "hover:bg-secondary-dark";
     const opacity = Math.max(0, Math.min(1, Math.pow(coords.y / (200 * scale), 2)));
 
     return (
@@ -15,9 +16,8 @@ export default function Checkpoint({ passed, coords, scale, size, borderWidth }:
                 height: `${size}px`,
                 borderWidth: `${borderWidth}px`,
                 opacity: opacity,
-                // boxShadow: "0 3px 0 0px rgb(107 114 128), 0 6px 0 0px rgb(107 114 128)",
             }}
-            className={`${color} ${hoverColor} rounded-full border-4 border-gray-500 transform translate-y-[-6px] active:translate-y-0 shadow-custom active:shadow-none`}
+            className={`${color} ${hoverColor} rounded-full border-4 border-dark transform translate-y-[-6px] active:translate-y-0 shadow-custom shadow-dark active:shadow-none`}
         >
         </button>
     );
