bc0bef67dfe02206310d7145d5831bb3bc5ff8d6
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-04-29 10:38:22 +0200

    darkmodetoggle: Removed unnecessary imports

--- src/app/components/settings/DarkModeToggle.tsx
+++ src/app/components/settings/DarkModeToggle.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { ThemeProvider } from "@/app/components/settings/ThemeProvider";
 import ThemeManager from "./ThemeManager";
 
 const DarkModeToggle = () => {
