9c6c4bff380ca941598232eb741bcd0e8068ebf6
Torgeir Haukaas <haukaas.torgeir@gmail.com>
2024-04-29 09:08:16 +0200

    api: fix api to work with real token and prod

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,22 +1,16 @@
 import axios from "axios";
 import { useAuth } from "@/context/AuthContext";
 
-// const host = "http://localhost";
-const https = "https://";
-const host = ".sysdevservices.tech/";
-
-function getAuthToken() {
-  return "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYjE2MzRmNC1lMzRhLTQ0NTMtYjgzNS04NTdlZTE4ZDViNmEiLCJpYXQiOjE3MTQwMzA0NDMsImV4cCI6MTcxNDA2NjQ0M30.HTDyw32mCeWjOz-tt93asrfvD03ouETOxg_6ZOKXH9I";
-}
+const protocol = "https://";
+const host = "ep.sysdevservices.tech/";
 
 export const useApiService = () => {
-  const  token  = useAuth();
+  const { token } = useAuth();
 
   const service = axios.create({
     baseURL: `${host}:8080`,
     headers: {
       "Content-Type": "application/json",
-      // ...(token ? { Authorization: `Bearer ${token}` } : {}),
     },
     timeout: 1000,
   });
@@ -38,10 +32,8 @@ export const useApiHandler = () => {
   const gatewayService = useApiService();
 
   const apiHandler = async (service, method, url, data = null) => {
-    const path = https + service + host + service + url;
-    const config = data
-      ? [path, data]
-      : [path];
+    const path = protocol + host + service + url;
+    const config = data ? [path, data] : [path];
 
     try {
       const response = await gatewayService[method](...config);
