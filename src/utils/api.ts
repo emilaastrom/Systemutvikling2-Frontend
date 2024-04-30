7a96f3fd6216d09dba9807a658592afea4a0ce98
Torgeir Haukaas <haukaas.torgeir@gmail.com>
2024-04-30 11:01:16 +0200

    api: add status response to api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,8 +15,9 @@ export const useApiService = (): AxiosInstance => {
     timeout: 1000,
   });
 
-service.interceptors.request.use(
-    (config: any): any => { // Using 'any' to avoid strict type errors
+  service.interceptors.request.use(
+    (config: any): any => {
+      // Using 'any' to avoid strict type errors
       // Ensure headers are properly initialized to an object if undefined
       config.headers = config.headers || {};
       if (token) {
@@ -39,21 +40,33 @@ export const useApiHandler = () => {
     method: "get" | "post" | "put" | "patch" | "delete",
     url: string,
     data: any = null,
-  ): Promise<any> => {
+  ): Promise<{ data: any; statusText: string; status: number }> => {
     const path: string = `${protocol}${host}${serviceName}${url}`;
 
     try {
       switch (method) {
-        case 'get':
-        case 'delete':
+        case "get":
+        case "delete": {
           // Direct method call with the path only
-          return (await gatewayService[method](path)).data;
+          const res = await gatewayService[method](path);
+          return {
+            data: res.data,
+            statusText: res.statusText,
+            status: res.status,
+          };
+        }
 
-        case 'post':
-        case 'put':
-        case 'patch':
+        case "post":
+        case "put":
+        case "patch": {
           // Method calls with path and data
-          return (await gatewayService[method](path, data)).data;
+          const res = await gatewayService[method](path, data);
+          return {
+            data: res.data,
+            statusText: res.statusText,
+            status: res.status,
+          };
+        }
       }
     } catch (error) {
       console.error(`Failed to ${method}: ${url}`, error);
