e09f98f566ce4fec72651e4cd283fe43fabfede4
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-04-30 11:02:57 +0200

    customizer-experience: Map ENUM values to correct language

--- src/app/components/settings/CustomizeExperience.tsx
+++ src/app/components/settings/CustomizeExperience.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-
+const languageMapping = {
+  EASY: "Enkel",
+  MEDIUM: "Medium",
+  HARD: "Vanskelig",
+};
   const challenges = [
     { id: 1, name: "Snus", emoji: "🚬" },       // Assuming emoji representation
     { id: 2, name: "Brus", emoji: "🥤" },
@@ -13,51 +15,7 @@ import { motion } from 'framer-motion';
     { id: 9, name: "Snop", emoji: "🍬" }
   ];
 
-    return (
-      <div className="flex flex-col items-center justify-center text-black p-4">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: isMounted ? 1 : 0 }}
-          transition={{ duration: 0.5 }}
-          className="text-center"
-        >
-          <h1 className="font-semibold text-2xl mb-4 dark:text-white">Sparepreferanser</h1>
-          <div className="flex space-x-2 justify-center mb-8">
-            {["Enkel", "Medium", "Vanskelig"].map((difficulty) => (
-              <button
-                key={difficulty}
-                onClick={() => setSelectedDifficulty(difficulty)}
-                className={`py-2 px-4 rounded-xl font-mono ${
-                  selectedDifficulty === difficulty
-                    ? "bg-green-500 text-white "
-                    : "bg-gray-200"
-                }`}
-              >
-                {difficulty}
-              </button>
-            ))}
-          </div>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-            {challenges.map((challenge) => (
-              <motion.div
-                key={challenge.id}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                className={`cursor-pointer p-2 border rounded-lg overflow-x-hidden ${
-                  selectedChallenges.has(challenge.id)
-                    ? "bg-green-200 border-green-500"
-                    : "bg-white border-gray-300"
-                }`}
-                onClick={() => toggleChallenge(challenge.id)}
-              >
-                <span className="text-xl font-mono">{challenge.emoji}</span>
-                <span className="ml-2">{challenge.name}</span>
-              </motion.div>
-            ))}
-          </div>
-        </motion.div>
-      </div>
-    );
+              {languageMapping[difficulty]}
 };
 
 export default CustomizeExperience;
