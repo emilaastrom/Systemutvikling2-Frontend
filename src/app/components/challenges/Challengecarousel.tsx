bacab8614183fb0eddcc55269d395a750f15bb4c
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-05-02 15:15:19 +0200

    scrollbar: Fixed css on all browsers + overflow-x-auto

--- src/app/components/challenges/Challengecarousel.tsx
+++ src/app/components/challenges/Challengecarousel.tsx
@@ -4,94 +4,101 @@ import { useApiHandler } from "@/utils/api";
 import { useEffect, useState } from "react";
 
 type Challenge = {
-  id: string,
-  name: string,
-  description: string,
-  durationDays: number,
-  type: string,
-  price: number
+    id: string;
+    name: string;
+    description: string;
+    durationDays: number;
+    type: string;
+    price: number;
 };
 
 type AssignedChallenge = {
-  id: string,
-  userId: string,
-  challengeId: string,
-  startDate: string,
-  endDate: string,
-  subStatus: boolean[],
-  completed: boolean,
-  finished: boolean,
-  amountSaved: number,
-  difficulty: string
-}
+    id: string;
+    userId: string;
+    challengeId: string;
+    startDate: string;
+    endDate: string;
+    subStatus: boolean[];
+    completed: boolean;
+    finished: boolean;
+    amountSaved: number;
+    difficulty: string;
+};
 
 type ActiveChallenge = {
-  challenge: Challenge,
-  assignedChallenge: AssignedChallenge,
-}
+    challenge: Challenge;
+    assignedChallenge: AssignedChallenge;
+};
 
 const Challengecarousel = () => {
-  const [activeChallenges, setActiveChallenges] = useState<ActiveChallenge[]>([]);
+    const [activeChallenges, setActiveChallenges] = useState<ActiveChallenge[]>(
+        []
+    );
 
-  const apiHandler = useApiHandler();
+    const apiHandler = useApiHandler();
 
-  const fetchChallenges = async () => {
-    try {
-      const challenges = await apiHandler("challenge", "get", "/getActiveChallenges");
-      setActiveChallenges(challenges.data)
-    } catch (error) {
-        console.error("Error fetching suggestions:", error);
-    }
-  }
+    const fetchChallenges = async () => {
+        try {
+            const challenges = await apiHandler(
+                "challenge",
+                "get",
+                "/getActiveChallenges"
+            );
+            setActiveChallenges(challenges.data);
+        } catch (error) {
+            console.error("Error fetching suggestions:", error);
+        }
+    };
 
-  useEffect(() => {
-    fetchChallenges();
-}, [])
-  
-  return (
-    <div
-      className="flex z-10 self-start w-screen text-black overflow-x-scroll overflow-y-auto no-scrollbar"
-    >
-      {activeChallenges.map((challenge, index) => {
-  var startDateParts = challenge.assignedChallenge.startDate.split('-');
-  var startYear = parseInt(startDateParts[0], 10);
-  var startMonth = parseInt(startDateParts[1], 10) - 1; 
-  var startDay = parseInt(startDateParts[2], 10);
-  var startDate = new Date(startYear, startMonth, startDay);
+    useEffect(() => {
+        fetchChallenges();
+    }, []);
 
-  var endDateParts = challenge.assignedChallenge.endDate.split('-');
-  var endYear = parseInt(endDateParts[0], 10);
-  var endMonth = parseInt(endDateParts[1], 10) - 1; 
-  var endDay = parseInt(endDateParts[2], 10);
-  var endDate = new Date(endYear, endMonth, endDay);
+    return (
+        <div className="flex z-10 self-start w-screen text-black overflow-x-auto overflow-y-auto customized-scrollbar">
+            {activeChallenges.map((challenge, index) => {
+                var startDateParts =
+                    challenge.assignedChallenge.startDate.split("-");
+                var startYear = parseInt(startDateParts[0], 10);
+                var startMonth = parseInt(startDateParts[1], 10) - 1;
+                var startDay = parseInt(startDateParts[2], 10);
+                var startDate = new Date(startYear, startMonth, startDay);
 
-  var numberOfTrue = challenge.assignedChallenge.subStatus.filter(function(value) {
-    return value === true;
-}).length;
+                var endDateParts =
+                    challenge.assignedChallenge.endDate.split("-");
+                var endYear = parseInt(endDateParts[0], 10);
+                var endMonth = parseInt(endDateParts[1], 10) - 1;
+                var endDay = parseInt(endDateParts[2], 10);
+                var endDate = new Date(endYear, endMonth, endDay);
 
+                var numberOfTrue = challenge.assignedChallenge.subStatus.filter(
+                    function (value) {
+                        return value === true;
+                    }
+                ).length;
 
-  return (
-    <div key={index} className="flex-none md:relative md:block">
-      <Challengecard
-        id={challenge.assignedChallenge.id}
-        challenge={challenge.challenge.description}
-        title={challenge.challenge.name}
-        current={numberOfTrue}
-        max={challenge.assignedChallenge.subStatus.length}
-        startDate={startDate}
-        endDate={endDate}
-        subStatus={challenge.assignedChallenge.subStatus}
-        difficulty={challenge.assignedChallenge.difficulty}
-      />
-    </div>
-  );
-})}
+                return (
+                    <div key={index} className="flex-none md:relative md:block">
+                        <Challengecard
+                            id={challenge.assignedChallenge.id}
+                            challenge={challenge.challenge.description}
+                            title={challenge.challenge.name}
+                            current={numberOfTrue}
+                            max={challenge.assignedChallenge.subStatus.length}
+                            startDate={startDate}
+                            endDate={endDate}
+                            subStatus={challenge.assignedChallenge.subStatus}
+                            difficulty={challenge.assignedChallenge.difficulty}
+                        />
+                    </div>
+                );
+            })}
 
-      <div className="m-4">
-      <ChallengecardAddButton reloadFunction={fetchChallenges} />
-      </div>
-    </div>
-  );
+            <div className="m-4">
+                <ChallengecardAddButton reloadFunction={fetchChallenges} />
+            </div>
+        </div>
+    );
 };
 
 export default Challengecarousel;
