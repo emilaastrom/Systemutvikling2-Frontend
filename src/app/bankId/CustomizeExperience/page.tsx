41aad48715530b84c65f15dc328b2c713b4e396a
Magnus411 <magnusgjerstad00@gmail.com>
2024-05-03 12:24:04 +0200

    fixing bankId routing

---
+++ src/app/bankId/CustomizeExperience/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CustomizeExperience from "../../components/settings/CustomizeExperience";
+function Page() {
+  return (
+    <div>
+      <CustomizeExperience
+        setSelectedDifficulty={undefined}
+        setSelectedChallenges={undefined}
+      />
+    </div>
+  );
+}
+
+export default Page;
