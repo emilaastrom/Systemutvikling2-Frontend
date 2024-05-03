f4f10f281ab0b0c7866043865eea7bc779f7bb33
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-05-03 10:20:35 +0200

    header: Dark mode toggle instead of streak

--- src/app/components/navigation/Header.tsx
+++ src/app/components/navigation/Header.tsx
@@ -1,71 +1,87 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { colors } from "../../../../tailwind.config";
-import { usePathname } from "next/navigation";
 import SvgIcon from "../icons/CustomIcon";
 import { useApiHandler } from "../../../utils/api";
 
 const CustomHeader = () => {
     const [progress, setProgress] = useState<number>();
     const [max, setMax] = useState<number>();
+    const [currentTheme, setCurrentTheme] = useState<string>("light"); // Default to 'light'
     const apiHandler = useApiHandler();
 
-    const fetchActiveGoal = async () => {
-        console.log("Fetching activegoal data");
-        try {
-            const data = await apiHandler("goal", "get", "/getActiveGoal");
-            if (data.status === 200) {
-                setProgress(data.data.progress);
-                setMax(data.data.amount);
+    useEffect(() => {
+        // Fetch goal and theme data only on mount or when apiHandler changes
+        const fetchData = async () => {
+            console.log("Fetching data");
+            try {
+                const [goalData, themeData] = await Promise.all([
+                    apiHandler("goal", "get", "/getActiveGoal"),
+                    apiHandler("user", "get", "/getUser"),
+                ]);
+
+                if (goalData.status === 200) {
+                    setProgress(goalData.data.progress);
+                    setMax(goalData.data.amount);
+                }
+
+                if (themeData.status === 200) {
+                    setCurrentTheme(themeData.data.theme);
+                }
+            } catch (error) {
+                console.error("Fetching data failed:", error);
             }
+        };
+
+        fetchData();
+    }, [apiHandler]);
+
+    const setThemeAPI = async (theme: string) => {
+        const string = theme.toUpperCase();
+        let themeData = { theme: string };
+        try {
+            await apiHandler("user", "put", "/updateUser", themeData);
+            setCurrentTheme(theme);
+            localStorage.setItem("theme", theme);
         } catch (error) {
-            console.error(error);
+            console.error("Setting theme failed:", error);
         }
     };
 
-    const pathname = usePathname();
+    const handleThemeChange = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const newTheme = currentTheme === "light" ? "dark" : "light";
+        setThemeAPI(newTheme);
+    };
 
-    if (
-        pathname === "/login" ||
-        pathname === "/bankId" ||
-        pathname === "/bankId/ChooseAccount"
-    ) {
-        return;
-    }
-
-    fetchActiveGoal();
     // Regular header for all other pages
     return (
         <header>
             <div className="fixed top-0 w-screen h-10 text-center bg-background-50 dark:bg-slate-600 dark:text-white z-20 drop-shadow-lg">
                 <div className="fixed flex align-middle left-5 w-auto  h-auto m-2 px-2 rounded-xl">
-                    <SvgIcon
-                        className="h-5 w-5"
-                        svg={
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke={colors.accent}
-                                className="w-6 h-6"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z"
-                                />
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z"
-                                />
-                            </svg>
-                        }
-                    />
-                    <h2 className="text-xl text-dark dark:text-white ml-2 align-text-top leading-tight">
-                        31
-                    </h2>
+                    <button onClick={handleThemeChange}>
+                        <SvgIcon
+                            className="h-5 w-5"
+                            svg={
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                                    />
+                                </svg>
+                            }
+                        />
+                    </button>
+                    {/* <h2 className="text-xl text-dark dark:text-white ml-2 align-text-top leading-tight">
+                        {currentTheme}
+                    </h2> */}
                 </div>
                 <div className="fixed flex right-5 w-auto h-fill align-middle text-black h-auto m-2 px-2 rounded-xl">
                     <SvgIcon
@@ -88,7 +104,7 @@ const CustomHeader = () => {
                         }
                     />
                     <span className="text-xl text-dark dark:text-white  ml-2 align-text-top leading-tight">
-                        {progress} / {max} kr
+                        {Math.floor(progress ?? 0)} / {max ?? 0} kr
                     </span>
                 </div>
             </div>
