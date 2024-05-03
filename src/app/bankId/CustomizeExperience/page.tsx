7427e6d3447634a4fde44af15eccbfa3f2077d5b
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-05-03 17:44:46 +0200

    customize-experience: Bugfix config component

--- src/app/bankId/CustomizeExperience/page.tsx
+++ src/app/bankId/CustomizeExperience/page.tsx
@@ -1,19 +1,81 @@
 "use client";
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import CustomizeExperience from "../../components/settings/CustomizeExperience";
+import { useApiHandler } from "../../../utils/api";
+import { useRouter } from "next/navigation";
+
+
 function Customize() {
-  return (
-    <div className="h-screen bg-white">
-      <div className="h-2/3 ">
-        <div className="pt-48">
-          <CustomizeExperience
-            setSelectedDifficulty={undefined}
-            setSelectedChallenges={undefined}
-          />
+    const [difficultyLevel, setDifficultyLevel] = useState<string>("");
+    const [selectedChallenges, setSelectedChallenges] = useState<string[]>([]);
+
+    const [initialDifficultyLevel, setInitialDifficultyLevel] = useState("");
+    const [tempChallenges, setTempChallenges] = useState<string[]>([]);
+
+    const apiHandler = useApiHandler();
+
+    const FetchUser = useCallback(async () => {
+        try {
+            const fetched = await apiHandler("user", "get", "/getUser");
+
+            setDifficultyLevel(fetched.data.difficultyLevel);
+            setInitialDifficultyLevel(fetched.data.difficultyLevel);
+
+            if (fetched.data.options) {
+                setSelectedChallenges([...fetched.data.options]);
+                setTempChallenges([...fetched.data.options]);
+            } else {
+                console.warn(
+                    "No challenges found in user data",
+                    fetched.data.options
+                );
+            }
+        } catch (error) {
+            console.error("Error fetching data from API: ", error);
+        }
+    }, [apiHandler]);
+
+    const handleDifficultyChange = (difficulty) => {
+        setDifficultyLevel(difficulty);
+    };
+
+    useEffect(() => {
+        if (!selectedChallenges) {
+            FetchUser();
+        }
+    });
+
+
+
+    const router = useRouter();
+
+    const handleStartClick = () => {
+      router.push("/dashboard");
+    };
+
+
+    return (
+        <div className="flex p-4 h-screen justify-center items-center content-center bg-white w-screen">
+            <div className="flex flex-col h-2/3 justify-center items-center">
+                <div className="pt-48">
+                    {/* <CustomizeExperience
+                        selectedDifficulty={`${difficultyLevel}`}
+                        setSelectedDifficulty={handleDifficultyChange}
+                        selectedChallenges={selectedChallenges}
+                        setSelectedChallenges={setSelectedChallenges}
+                    /> */}
+
+                    <CustomizeExperience
+                        selectedDifficulty={`${difficultyLevel}`}
+                        setSelectedDifficulty={handleDifficultyChange}
+                        selectedChallenges={undefined}
+                        setSelectedChallenges={undefined}
+                    />
+                </div>
+                <button className="button-enticing" onClick={handleStartClick}>Start din Sparesti</button>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Customize;
